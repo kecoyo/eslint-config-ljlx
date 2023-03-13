133de143d90fbaa9b8cc59a3cc920e67b1fb5591
杨可可 <kecoyo@163.com>
2023-03-13 10:39:55 +0800

    1.0.2

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ module.exports = {
     "no-shadow": 0, // 旨在消除阴影变量声明
     "no-underscore-dangle": 0, // 不允许在标识符中使用悬空下划线
     "no-unused-expressions": 0, // 旨在消除对程序状态没有影响的未使用的表达式
-    "no-unused-vars": 0, // 旨在消除未使用的变量，函数和函数的参数
+    "no-unused-vars": 1, // 旨在消除未使用的变量，函数和函数的参数
     "no-use-before-define": 0, // 会在遇到对尚未声明的标识符的引用时发出警告
     "object-curly-newline": 0, // 在对象文字或解构赋值的大括号内强制执行一致的换行符
     "object-shorthand": 0, // 强制使用简写语法
